47256e615e03f91dac0ef37578a26797333f6905
ezoza05 <101642184+ezoza05@users.noreply.github.com>
2023-02-13 00:09:54 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+     var ask = prompt('solve this given 5i<15u')
+     if( ask == "i<3u"){
+         alert('i<3 u too:3')
+     }
+     else if(ask == NaN){
+     alert('go learn math')
+    }
+    else{
+       alert("dont be lazy")
+       alert('try')
+       var ask1 = prompt('5i<15u')
+       if( ask1 == "i<3u"){
+         alert('i<3 u too:3')
+     }
+     else if(ask1 == NaN){
+     alert('go learn math')
+     }
+    }
